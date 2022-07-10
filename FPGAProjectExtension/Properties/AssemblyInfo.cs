93824566e77f6c7334a68506d345a283db3b85f3
jobmarley <jobmarleythetruebob@protonmail.com>
2022-07-11 00:27:05 +0200

    rename fpga project p1

--- FPGAProjectExtension/Properties/AssemblyInfo.cs
+++ FPGAProjectExtension/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("FPGAProjectExtension")]
+[assembly: AssemblyTitle("VSMipsProjectExtension")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("FPGAProjectExtension")]
+[assembly: AssemblyProduct("VSMipsProjectExtension")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
